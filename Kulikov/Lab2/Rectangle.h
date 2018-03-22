ce08a5fb449f22d938d123fb16c74eca3049abc9
RamonaFlow3rs <maximko98@gmail.com>
2018-03-23 01:49:03 +0300

    Update Rectangle.h

--- Kulikov/Lab2/Rectangle.h
+++ Kulikov/Lab2/Rectangle.h
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Rectangle.h
- * Author: ekulikov
- *
- * Created on 8 августа 2017 г., 16:48
- */
-
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
 #include "Shape.h"
 #include <utility>
+#include "exc.h"
 #include <iostream>
 
 using namespace std;
@@ -23,30 +11,29 @@ using namespace std;
 class Rectangle : public Shape {
 public:
     
-    Rectangle(pair <double, double> a, pair<double, double> b, pair<double, double> c, pair<double, double> d);
-    virtual ~Rectangle();
+    Rectangle(const Point a, const Point b, const Point c, const Point d);
+    ~Rectangle();
     
     
-    void move(Point newP) override;
+    void move(const Point newP) override;
     void turn(double angle) override;
     void scale(double factor) override;
     void setColor(std::string color) override;
-    string getId() override;
+    int getId() override;
     string getColor() override;
     friend ostream& operator<<(ostream& os, const Rectangle& rect);
-    
     
 private:     
     Point positionCentre();
     
-    
+   
 private:
-    pair <double, double> _a;
-    pair <double, double> _b;
-    pair <double, double> _c;
-    pair <double, double> _d;
+    Point _a;
+    Point _b;
+    Point _c;
+    Point _d;
     string _color;
-
+    uint32_t _id;
 };
 
 #endif /* RECTANGLE_H */
