8ffd60a0462beb6b0825e217af3ab348cd3c1a2d
RamonaFlow3rs <maximko98@gmail.com>
2018-03-23 01:52:51 +0300

    Update Trapeze.h

--- Kulikov/Lab2/Trapeze.h
+++ Kulikov/Lab2/Trapeze.h
@@ -1,45 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Trapeze.h
- * Author: ekulikov
- *
- * Created on 15 марта 2018 г., 17:31
- */
-
 #ifndef TRAPEZE_H
 #define TRAPEZE_H
 #include "Shape.h"
+#include "exc.h"
 #include <utility>
 
 using namespace std;
 
 class Trapeze : public Shape {
 public:
-    Trapeze(pair <double, double> a, pair<double, double> b, pair<double, double> c, pair<double, double> d);
-    virtual ~Trapeze();
+    Trapeze(const Point a, const Point b, const Point c, const Point d);
+    ~Trapeze();
     
-    void move(Point newP) override;
+    void move(const Point newP) override;
     void turn(double angle) override;
     void scale(double factor) override;
-    void setColor(std::string color) override;
+    void setColor(string color) override;
     string getColor() override;
-    string getId() override;
+    int getId() override;
     friend ostream& operator<<(ostream& os, const Trapeze& rect);
+
     
 private:
     Point positionCentre();
+
     
 private:
-    pair <double, double> _a;
-    pair <double, double> _b;
-    pair <double, double> _c;
-    pair <double, double> _d;
+    Point _a;
+    Point _b;
+    Point _c;
+    Point _d;
     string _color;
+    uint32_t _id;
 };
 
 #endif /* TRAPEZE_H */
