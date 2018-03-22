aad73141c9382dd39b64505b2485333f3da39605
RamonaFlow3rs <maximko98@gmail.com>
2018-03-23 01:50:23 +0300

    Update RegularPentagon.h

--- Kulikov/Lab2/RegularPentagon.h
+++ Kulikov/Lab2/RegularPentagon.h
@@ -1,48 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   RegularPentagon.h
- * Author: ekulikov
- *
- * Created on 15 марта 2018 г., 17:33
- */
-
 #ifndef REGULARPENTAGON_H
 #define REGULARPENTAGON_H
 
 #include "Shape.h"
+#include "exc.h"
 #include <utility>
 
 using namespace std;
 
 class RegularPentagon : public Shape {
 public:
-    RegularPentagon(pair <double, double> a, pair<double, double> b, pair<double, double> c, pair<double, double> d, pair<double, double> e);
-    virtual ~RegularPentagon();
+    RegularPentagon(const Point a, const Point b, const Point c, const Point d, const Point e);
+    ~RegularPentagon();
     
-    void move(Point newP) override;
+    void move(const Point newP) override;
     void turn(double angle) override;
     void scale(double factor) override;
     void setColor(std::string color) override;
     string getColor() override;
-    string getId() override;
+    int getId() override;
     friend ostream& operator<<(ostream& os, const RegularPentagon& pent);
     
 private:
     Point positionCentre();
+ 
   
     
 private:
-    pair <double, double> _a;
-    pair <double, double> _b;
-    pair <double, double> _c;
-    pair <double, double> _d;
-    pair <double, double> _e;
+    Point _a;
+    Point _b;
+    Point _c;
+    Point _d;
+    Point _e;
     string _color;
+    uint32_t _id;
 };
 
 #endif /* REGULARPENTAGON_H */
