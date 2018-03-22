4752d181a139b4039655b5ed2f4b23951c01a5c5
RamonaFlow3rs <maximko98@gmail.com>
2018-03-23 01:51:09 +0300

    Update Shape.h

--- Kulikov/Lab2/Shape.h
+++ Kulikov/Lab2/Shape.h
@@ -1,20 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   IFigures.h
- * Author: ekulikov
- *
- * Created on 7 августа 2017 г., 17:00
- */
-
 #ifndef SHAPE_H
 #define SHAPE_H
 
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -23,16 +11,20 @@ struct Point {
  double y;
 
  Point(): x(0.0), y(0.0) {}
+ Point(double x_, double y_): x(x_), y(y_) {}
 };
 
 class Shape {
 public:
-    virtual void move(Point newP) = 0;
+    virtual void move(const Point newP) = 0;
     virtual void turn(double angle) = 0;
     virtual void scale(double factor) = 0;
     virtual void setColor(string color) = 0;
     virtual string getColor() = 0;
-    virtual string getId() = 0;
+    virtual int getId() = 0;
+
+protected:
+    static uint32_t sCounter;
 
 };
 
