270bd8526388f15088862ee5c19869198207f807
RamonaFlow3rs <maximko98@gmail.com>
2018-03-29 23:46:17 +0300

    Update Rectangle.cpp

--- Kulikov/Lab2/Rectangle.cpp
+++ Kulikov/Lab2/Rectangle.cpp
@@ -11,16 +11,17 @@ Rectangle::Rectangle(const Point a, const Point b, const Point c, const Point d)
         cout << "The new Rectangle has been created!" << endl;
         
     } else {
-        throw badRectangleException();   
+        throw BadFigureException("Rectangle");   
     }
-
-    _id = Shape::sCounter++;
+    
+    _id = getNextId(); 
 }
 
 Rectangle::~Rectangle() {
 }
 
-int Rectangle::getId(){
+
+uint32_t Rectangle::id(){
     return _id;
 }
 
@@ -47,40 +48,68 @@ void Rectangle::move(const Point newP) {
 
 
 void Rectangle::turn(double angle){
+    Point centre = positionCentre();
+    
+    angle =  fmod(angle, 360);
     angle *= M_PI/180;
     
     Point tmp;
     
-    tmp.x = _a.x*cos(angle) - _a.y*sin(angle);
-    tmp.y = _a.x*sin(angle) + _a.y*cos(angle); 
-    _a.x = tmp.x;
-    _a.y = tmp.y;
+    //X = x0 + (x - x0) * cos(a) - (y - y0) * sin(a);
+    //Y = y0 + (x - x0) * sin(a) + (y - y0) * cos(a);
     
-    
-    tmp.x = _b.x*cos(angle) - _b.y*sin(angle);
-    tmp.y = _b.x*sin(angle) + _b.y*cos(angle); 
-    _b.x = tmp.x;
-    _b.y = tmp.y;
-    
-    
-    tmp.x = _c.x*cos(angle) - _c.y*sin(angle);
-    tmp.y = _c.x*sin(angle) + _c.y*cos(angle); 
-    _c.x = tmp.x;
-    _c.y = tmp.y;
-    
-    
-    tmp.x = _d.x*cos(angle) - _d.y*sin(angle);
-    tmp.y = _d.x*sin(angle) + _d.y*cos(angle); 
-    _d.x = tmp.x;
-    _d.y = tmp.y; 
+    tmp.x = centre.x + (_a.x - centre.x)*cos(angle) - (_a.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_a.x - centre.x)*sin(angle) + (_a.y - centre.y)*cos(angle);
+    _a.x = round(tmp.x*10000)/10000;
+    _a.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_b.x - centre.x)*cos(angle) - (_b.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_b.x - centre.x)*sin(angle) + (_b.y - centre.y)*cos(angle);
+    _b.x = round(tmp.x*10000)/10000;
+    _b.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_c.x - centre.x)*cos(angle) - (_c.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_c.x - centre.x)*sin(angle) + (_c.y - centre.y)*cos(angle);
+    _c.x = round(tmp.x*10000)/10000;
+    _c.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_d.x - centre.x)*cos(angle) - (_d.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_d.x - centre.x)*sin(angle) + (_d.y - centre.y)*cos(angle);
+    _d.x = round(tmp.x*10000)/10000;
+    _d.y = round(tmp.y*10000)/10000;    
 }
 
 
 void Rectangle::scale(double factor){
-    _a.x *= factor; _a.y *= factor;
-    _b.x *= factor; _b.y *= factor;
-    _c.x *= factor; _c.y *= factor;
-    _d.x *= factor; _d.y *= factor;
+    Point centre = positionCentre();
+    
+    vector <double> xArray = {_a.x, _b.x, _c.x, _d.x};
+    vector <double> yArray = {_a.y, _b.y, _c.y, _d.y};
+     
+    for (int i = 0; i < xArray.capacity() ; i++) {
+        if (xArray[i] > centre.x) {
+            xArray[i] = centre.x + (xArray[i] - centre.x) * factor;
+            
+        } else {
+            xArray[i] = centre.x - (centre.x - xArray[i]) * factor; 
+        }
+    }
+    
+    for (int i = 0; i < yArray.capacity() ; i++) {
+        if (yArray[i] > centre.y) {
+            yArray[i] = centre.y + (yArray[i] - centre.y) * factor; 
+        } else {
+            yArray[i] = centre.y - (centre.y - yArray[i]) * factor; 
+        }
+    }
+
+    _a.x = xArray[0]; _a.y = yArray[0];
+    _b.x = xArray[1]; _b.y = yArray[1];
+    _c.x = xArray[2]; _c.y = yArray[2];
+    _d.x = xArray[3]; _d.y = yArray[3];
 }
 
 void Rectangle::setColor(string color) {
@@ -95,4 +124,5 @@ ostream& operator<<(ostream& os, const Rectangle& rect){
     os << '(' << rect._a.x << ':' << rect._a.y << ')' << endl << '(' << rect._b.x << ':' << rect._b.y << ')' << endl << '(' << rect._c.x << ':' << rect._c.y << ')' << endl << '(' << rect._d.x << ':' << rect._d.y << ')' << endl;
             
     return os;
+
 }
