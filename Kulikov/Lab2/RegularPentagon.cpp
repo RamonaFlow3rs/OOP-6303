32d6ebbc4786c38875f540758c4c68231c2190ec
RamonaFlow3rs <maximko98@gmail.com>
2018-03-29 23:47:59 +0300

    Update RegularPentagon.cpp

--- Kulikov/Lab2/RegularPentagon.cpp
+++ Kulikov/Lab2/RegularPentagon.cpp
@@ -4,9 +4,7 @@
 
 using namespace std;
 
-
 RegularPentagon::RegularPentagon(const Point a, const Point b, const Point c, const Point d, const Point e) : _a(a), _b(b), _c(c), _d(d) , _e(e) {
-    
     double m1=round(sqrt(pow(((c.x+d.x)/2-a.x),2)+pow(((c.y+d.y)/2-a.y),2)) * 100)/100;
     double m2=round(sqrt(pow(((d.x+e.x)/2-b.x),2)+pow(((d.y+e.y)/2-b.y),2))*100) /100;
     double m3=round(sqrt(pow(((a.x+e.x)/2-c.x),2)+pow(((a.y+e.y)/2-c.y),2))*100)/100;
@@ -14,11 +12,12 @@ RegularPentagon::RegularPentagon(const Point a, const Point b, const Point c, co
     double m5=round(sqrt(pow(((c.x+b.x)/2-e.x),2)+pow(((c.y+b.y)/2-e.y),2))*100)/100;
 
     if(!(m1==m2 && m1==m3 && m1==m4 && m1==m5)){
-        throw badPentagonException();
+        throw BadFigureException("RegularPentagon");
     }
     
-    
-  _id = Shape::sCounter++;
+  cout << "The new Regular Pentagon has been created!" << endl;  
+  _id = getNextId();
+
 }
 
 
@@ -26,34 +25,45 @@ RegularPentagon::~RegularPentagon() {
 }
 
 
-int RegularPentagon::getId(){
+uint32_t RegularPentagon::id(){
     return _id;
 }
 
 Point RegularPentagon::positionCentre(){
-    Point A; Point C;
-  
-    A.x = min((min(_a.x, _b.x), min(_c.x, _d.x)), _e.x);
-    A.y = min((min(_a.y, _b.y), min(_c.y, _d.y)), _e.y);
+
+        Point centre;
+        
+        double A1 = ((_a.y + _b.y)/2) - _d.y;
+        double B1 =  _d.x - ((_a.x + _b.x)/2);
+        double C1 = ((_a.x + _b.x)/2)*_d.y - _d.x*((_a.y + _b.y)/2) ;
+        
+        double A2 = (_d.y + _e.y)/2 - _b.y;
+        double B2 = _b.x - (_d.x + _e.x)/2 ;
+        double C2 = ((_d.x + _e.x)/2)*_b.y - _b.x*((_d.y + _e.y)/2) ;
+        
+        
+        //        A1*x  +  B1*y + C1 = 0; 
+        //        A2*x  +  B2*y + C2 = 0;
+        
+        //        A1*x  +  B1*y = -1*C1; 
+        //        A2*x  +  B2*y = -1*C2;
+        
+        double delta    = A1 * B2 - B1 * A2;
+        double delta_x  = (-1)*C1 * B2 - B1 * (-1)*C2;  
+        double delta_y  = A1 * (-1)*C2 - (-1)*C1 * A2; 
+        
+        centre.x = delta_x / delta;
+        centre.y = delta_y / delta;    
     
-    C.x = max((max(_a.x, _b.x), max(_c.x, _d.x)), _e.x);
-    C.y = max((max(_a.y, _b.y), max(_c.y, _d.y)), _e.y);   
-    
-    
-    double xc = (C.x - A.x) / 2;
-    double yc = (C.y - A.y) / 2;
-    Point centre;
-    centre.x = xc;
-    centre.y = yc;
-    return centre;
+        return centre;
 }
 
 
 void RegularPentagon::move(const Point newP){
     Point centre = positionCentre();
     
-    double movedX = newP.x ;
-    double movedY = newP.y;
+    double movedX = newP.x - centre.x;
+    double movedY = newP.y - centre.y;
     _a.x += movedX; _a.y += movedY;
     _b.x += movedX; _b.y += movedY;
     _c.x += movedX; _c.y += movedY;        
@@ -62,47 +72,76 @@ void RegularPentagon::move(const Point newP){
 
 
 void RegularPentagon::turn(double angle){
+    Point centre = positionCentre();
+    
+    angle =  fmod(angle, 360);
     angle *= M_PI/180;
     
     Point tmp;
     
-    tmp.x = _a.x*cos(angle) - _a.y*sin(angle);
-    tmp.y = _a.x*sin(angle) + _a.y*cos(angle); 
-    _a.x = tmp.x;
-    _a.y = tmp.y;
-    
-    
-    tmp.x = _b.x*cos(angle) - _b.y*sin(angle);
-    tmp.y = _b.x*sin(angle) + _b.y*cos(angle); 
-    _b.x = tmp.x;
-    _b.y = tmp.y;
-    
-    
-    tmp.x = _c.x*cos(angle) - _c.y*sin(angle);
-    tmp.y = _c.x*sin(angle) + _c.y*cos(angle); 
-    _c.x = tmp.x;
-    _c.y = tmp.y;
-    
-    
-    tmp.x = _d.x*cos(angle) - _d.y*sin(angle);
-    tmp.y = _d.x*sin(angle) + _d.y*cos(angle); 
-    _d.x = tmp.x;
-    _d.y = tmp.y;  
-    
-    
-    tmp.x = _e.x*cos(angle) - _e.y*sin(angle);
-    tmp.y = _e.x*sin(angle) + _e.y*cos(angle); 
-    _e.x = tmp.x;
-    _e.y = tmp.y;
+    //X = x0 + (x - x0) * cos(a) - (y - y0) * sin(a);
+    //Y = y0 + (x - x0) * sin(a) + (y - y0) * cos(a);
+
+    tmp.x = centre.x + (_a.x - centre.x)*cos(angle) - (_a.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_a.x - centre.x)*sin(angle) + (_a.y - centre.y)*cos(angle);
+    _a.x = round(tmp.x*10000)/10000;
+    _a.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_b.x - centre.x)*cos(angle) - (_b.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_b.x - centre.x)*sin(angle) + (_b.y - centre.y)*cos(angle);
+    _b.x = round(tmp.x*10000)/10000;
+    _b.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_c.x - centre.x)*cos(angle) - (_c.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_c.x - centre.x)*sin(angle) + (_c.y - centre.y)*cos(angle);
+    _c.x = round(tmp.x*10000)/10000;
+    _c.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_d.x - centre.x)*cos(angle) - (_d.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_d.x - centre.x)*sin(angle) + (_d.y - centre.y)*cos(angle);
+    _d.x = round(tmp.x*10000)/10000;
+    _d.y = round(tmp.y*10000)/10000;
+
+
+    tmp.x = centre.x + (_e.x - centre.x)*cos(angle) - (_e.y - centre.y)*sin(angle);
+    tmp.y = centre.y + (_e.x - centre.x)*sin(angle) + (_e.y - centre.y)*cos(angle);
+    _e.x = round(tmp.x*10000)/10000;
+    _e.y = round(tmp.y*10000)/10000;
 }
 
 
 void RegularPentagon::scale(double factor){
-    _a.x *= factor; _a.y *= factor;
-    _b.x *= factor; _b.y *= factor;
-    _c.x *= factor; _c.y *= factor;
-    _d.x *= factor; _d.y *= factor;
-    _e.x *= factor; _e.y *= factor;
+Point centre = positionCentre();
+    
+    vector <double> xArray = {_a.x, _b.x, _c.x, _d.x, _e.x};
+    vector <double> yArray = {_a.y, _b.y, _c.y, _d.y, _e.y};
+     
+    for (int i = 0; i < xArray.capacity() ; i++) {
+        if (xArray[i] > centre.x) {
+            xArray[i] = centre.x + (xArray[i] - centre.x) * factor;
+            
+        } else {
+            xArray[i] = centre.x - (centre.x - xArray[i]) * factor; 
+        }
+    }
+    
+    for (int i = 0; i < yArray.capacity() ; i++) {
+        if (yArray[i] > centre.y) {
+            yArray[i] = centre.y + (yArray[i] - centre.y) * factor; 
+        } else {
+            yArray[i] = centre.y - (centre.y - yArray[i]) * factor; 
+        }
+    }
+    
+    
+    _a.x = xArray[0]; _a.y = yArray[0];
+    _b.x = xArray[1]; _b.y = yArray[1];
+    _c.x = xArray[2]; _c.y = yArray[2];
+    _d.x = xArray[3]; _d.y = yArray[3];
+    _e.x = xArray[4]; _e.y = yArray[4]; 
 }
 
 
